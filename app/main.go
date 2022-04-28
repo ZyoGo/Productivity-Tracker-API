eb554e63caf112dbf266c53200a731fb224239f4
w33h <academyynetworking@gmail.com>
2022-04-28 13:55:08 +0800

    fix: fix update on shutdown server

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
@@ -33,8 +35,12 @@ func main() {
 	// Block until we receive our signal
 	<-quit
 
+	// timeout to wait for server shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	// Shutdown server
-	if err := e.Shutdown(nil); err != nil {
+	if err := e.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
