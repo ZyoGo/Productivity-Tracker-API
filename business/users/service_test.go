e16d0c0197e7c7b898074e0418016f195466f23a
w33h <academyynetworking@gmail.com>
2022-05-18 02:50:09 +0800

    fix: fixing bug unit testing on feature user

--- business/users/service_test.go
+++ business/users/service_test.go
@@ -2,131 +2,186 @@ package user_test
 
 import (
 	"errors"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"github.com/w33h/Productivity-Tracker-API/business/users/spec"
 	"github.com/w33h/Productivity-Tracker-API/repository/users/mocks"
 	"testing"
 	"time"
 
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	user "github.com/w33h/Productivity-Tracker-API/business/users"
-	"github.com/w33h/Productivity-Tracker-API/business/users/spec"
 )
 
-var userRepository = mocks.RepositoryUser{}
-var userService = user.NewUserService(&userRepository)
-
-func TestUserService_UpdateUser(t *testing.T) {
-	t.Run("Update user with valid request", func(t *testing.T) {
-		validRequest := user.Users{
-			Id:          "1",
-			Username:    "user1@gmail.com",
-			Password:    "user123456",
-			PhoneNumber: 628222222222,
-			CreatedAt:   time.Now(),
-			LastLogin:   time.Now(),
-			Deleted:     false,
-		}
-
-		userSpec := spec.UpsertUserSpec{
-			Username:    "user123@gmail.com",
-			Password:    "user123456",
-			PhoneNumber: 6282394707112,
-		}
-
-		userRepository.On("FindById", mock.Anything).Return(&validRequest, nil)
-		userRepository.On("UpdateUser", mock.Anything).Return(nil)
-		err := userService.UpdateUser(userSpec, "1")
-		//fmt.Println("error = ", err.Error)
-		//expectedError := errors.New("test")
-
-		assert.NoError(t, err)
-		//assert.Equal(t, expectedError, err)
-	})
+type UnitTestSuite struct {
+	suite.Suite
+	userService user.ServiceUser
+	userMock *mocks.RepositoryUser
 }
 
-func TestUserService_Delete(t *testing.T) {
-	t.Run("Delete user with invalid id", func(t *testing.T) {
-		userRepository.On("DeleteUser", mock.Anything).Return(errors.New("failed")).Once()
-		err := userService.DeleteUser("1")
-
-		assert.Error(t, err)
-	})
-
-	t.Run("Delete user with valid id", func(t *testing.T) {
-		userRepository.On("DeleteUser", mock.Anything).Return(nil)
-		err := userService.DeleteUser("1")
-
-		assert.NoError(t, err)
-	})
+func TestUnitTestSuite(t *testing.T) {
+	suite.Run(t, &UnitTestSuite{})
 }
 
-func TestUserService_Create(t *testing.T) {
-	t.Run("Create user with valid request", func(t *testing.T) {
-		validRequest := user.Users{
-			Id:          uuid.New().String(),
-			Username:    "usera@gmail.com",
-			Password:    "usera123",
-			PhoneNumber: +6282394707112,
-			CreatedAt:   time.Now(),
-			LastLogin:   time.Now(),
-			Deleted:     false,
-		}
+func (s *UnitTestSuite) SetupTest()  {
+	userMocks := mocks.RepositoryUser{}
+	userServices := user.NewUserService(&userMocks)
 
-		userSpec := spec.UpsertUserSpec{
-			Username:    "usera@gmail.com",
-			Password:    "usera123",
-			PhoneNumber: +6282394707112,
-		}
-
-		userRepository.Mock.On("InsertUser", mock.Anything).Return(validRequest, nil)
-		result, err := userService.CreateUser(userSpec)
-
-		assert.NoError(t, err)
-		assert.NotNil(t, result)
-	})
-
-	t.Run("Create user with invalid request", func(t *testing.T) {
-		invalidResponse := user.Users{}
-
-		userSpecInvalid := spec.UpsertUserSpec{
-			Username:    "usera@gmail.com",
-			Password:    "123",
-			PhoneNumber: +6282394707112,
-		}
-
-		userRepository.Mock.On("InsertUser", mock.Anything).Return(invalidResponse, nil)
-		result, err := userService.CreateUser(userSpecInvalid)
-
-		assert.Error(t, err)
-		assert.NotNil(t, result)
-	})
+	s.userService = userServices
+	s.userMock = &userMocks
 }
 
-func TestUserService_Get(t *testing.T) {
-	t.Run("Get user with valid id", func(t *testing.T) {
-		validRequest := user.Users{
-			Id:          "1",
-			Username:    "user1@gmail.com",
-			Password:    "user123456",
-			PhoneNumber: +628222222222,
-			CreatedAt:   time.Now(),
-			LastLogin:   time.Now(),
-			Deleted:     false,
-		}
+func (s *UnitTestSuite) TestUserService_Delete_Success()  {
+		s.userMock.On("DeleteUser", mock.Anything).Return(nil)
+		err := s.userService.DeleteUser("1")
 
-		userRepository.On("FindById", mock.Anything).Return(&validRequest, nil)
-		result, err := userService.GetById("1")
+		assert.NoError(s.T(), err)
+}
 
-		assert.NoError(t, err)
-		assert.NotNil(t, result)
-	})
+func (s *UnitTestSuite) TestUserService_Delete_Failed()  {
+	s.userMock.On("DeleteUser", mock.Anything).Return(errors.New("Failed"))
+	err := s.userService.DeleteUser("1")
 
-	t.Run("Get user with invalid id", func(t *testing.T) {
-		userRepository.On("FindById", "12").Return(nil, errors.New("failed"))
-		resultService, err := userService.GetById("12")
+	assert.Error(s.T(), err)
+}
 
-		assert.Error(t, err)
-		assert.Nil(t, resultService)
-	})
+func (s *UnitTestSuite) TestUserService_Create_Success()  {
+	validRequest := user.Users{
+					Id:          uuid.New().String(),
+					Username:    "userA@gmail.com",
+					Password:    "userA123",
+					PhoneNumber: +6280000000000,
+					CreatedAt:   time.Now(),
+					LastLogin:   time.Now(),
+					Deleted:     false,
+				}
+	userSpec := spec.UpsertUserSpec{
+					Username:    "userA@gmail.com",
+					Password:    "userA123",
+					PhoneNumber: +6280000000000,
+				}
+
+	s.userMock.On("InsertUser", mock.Anything).Return(validRequest, nil)
+	result, err := s.userService.CreateUser(userSpec)
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), result)
+}
+
+func (s *UnitTestSuite) TestUserService_Create_InternalServerFailed()  {
+	invalidRequest := user.Users{}
+	userSpec := spec.UpsertUserSpec{
+		Username:    "userA@gmail.com",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+	}
+
+	s.userMock.On("InsertUser", mock.Anything).Return(invalidRequest, errors.New("internal server failed"))
+	result, err := s.userService.CreateUser(userSpec)
+
+	assert.Error(s.T(), err)
+	assert.NotNil(s.T(), result)
+}
+
+func (s *UnitTestSuite) TestUserService_Create_BadRequest()  {
+	invalidRequest := user.Users{
+		Id:          "1",
+		Username:    "userA@gmail.com",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+		CreatedAt:   time.Now(),
+		LastLogin:   time.Now(),
+		Deleted:     false,
+	}
+	userSpec := spec.UpsertUserSpec{
+		Username:    "userA",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+	}
+
+	s.userMock.On("InsertUser", mock.Anything).Return(invalidRequest, errors.New("user spec failed"))
+	result, err := s.userService.CreateUser(userSpec)
+
+	assert.Error(s.T(), err)
+	assert.NotNil(s.T(), result)
+}
+
+func (s *UnitTestSuite) TestUserService_Get_Success() {
+	s.userMock.On("FindById", "1").Return(&user.Users{}, nil)
+	result, err := s.userService.GetById("1")
+
+	assert.NoError(s.T(), err)
+	assert.NotNil(s.T(), result)
+}
+
+func (s *UnitTestSuite) TestUserService_Get_Failed_InvalidID() {
+	s.userMock.On("FindById", "1").Return(nil, errors.New("id not found"))
+	result, err := s.userService.GetById("1")
+
+	assert.Error(s.T(), err)
+	assert.Nil(s.T(), result)
+}
+
+func (s *UnitTestSuite) TestUserService_Update_Success() {
+	invalidRequest := user.Users{
+		Id:          "1",
+		Username:    "userA@gmail.com",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+		CreatedAt:   time.Now(),
+		LastLogin:   time.Now(),
+		Deleted:     false,
+	}
+	userSpec := spec.UpsertUserSpec{
+		Username:    "userA@gmail.com",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+	}
+
+	s.userMock.On("FindById", mock.Anything).Return(&invalidRequest, nil)
+	s.userMock.On("UpdateUser", mock.Anything).Return(nil)
+
+	err := s.userService.UpdateUser(userSpec, "1")
+
+	assert.NoError(s.T(), err)
+}
+
+func (s *UnitTestSuite) TestUserService_Update_Failed_InternalServerFailed() {
+	userSpec := spec.UpsertUserSpec{
+		Username:    "userA@gmail.com",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+	}
+
+	s.userMock.On("FindById", mock.Anything).Return(nil, errors.New("id not found"))
+	s.userMock.On("UpdateUser", mock.Anything).Return(nil)
+
+	err := s.userService.UpdateUser(userSpec, "1")
+
+	assert.Error(s.T(), err)
+}
+
+func (s *UnitTestSuite) TestUserService_Update_Failed_BadRequest() {
+	invalidRequest := user.Users{
+		Id:          "1",
+		Username:    "userA@gmail.com",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+		CreatedAt:   time.Now(),
+		LastLogin:   time.Now(),
+		Deleted:     false,
+	}
+	userSpec := spec.UpsertUserSpec{
+		Username:    "userA",
+		Password:    "userA123",
+		PhoneNumber: +6280000000000,
+	}
+
+	s.userMock.On("FindById", mock.Anything).Return(&invalidRequest, nil)
+	s.userMock.On("UpdateUser", mock.Anything).Return(errors.New("bad request"))
+
+	err := s.userService.UpdateUser(userSpec, "1")
+
+	assert.Error(s.T(), err)
 }
